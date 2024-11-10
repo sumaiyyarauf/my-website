36f70bf6ebd1a5fae9fe60aedaaf51a5f249042e
sumaiyasheikh088 <sumaiyasheikh088@gmail.com>
2024-11-10 21:16:46 +0530

    2nd

--- src/Component/Pricing.jsx
+++ src/Component/Pricing.jsx
@@ -5,7 +5,7 @@ const Pricing = () => {
     <section id="pricing" className="py-20 text-center bg-yellow-100">
       <h2 className="text-[50px] font-bold mb-6">Delicious <span className='text-yellow-500'>Choices</span> You’ll Love</h2>
 
-      <div className="container mx-auto flex gap-8 justify-center">
+      <div className="container mx-auto flex flex-col sm:flex-row gap-8 justify-center">
         <div className="bg-yellow-200 p-6 rounded-lg w-full sm:w-1/3 transition-transform transform hover:scale-105 hover:shadow-lg">
           <img
             src="https://images.pexels.com/photos/16474895/pexels-photo-16474895/free-photo-of-roasted-chicken-and-salads-on-the-table.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
